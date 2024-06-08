bc9ee4721a40a4bc20a0416680b7005265167c7e
Tessa <48088982+Tesax123@users.noreply.github.com>
2024-06-08 12:23:39 +0200

    Update b_sterrenhemel.py: added missing import turtle

--- oplossingen/b_sterrenhemel.py
+++ oplossingen/b_sterrenhemel.py
@@ -1,4 +1,5 @@
 import random
+import turtle
 from sterren_module import *
 
 kleuren = ("red", "orange", "yellow", "lime green", "orchid", "magenta", "dodger blue", "crimson", "chocolate", "navy", "salmon", "green yellow", "teal", "cyan", "aquamarine", "hot pink", "firebrick", "royal blue", "wheat")
